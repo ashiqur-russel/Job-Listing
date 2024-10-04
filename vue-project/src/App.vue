36ce12c9cb2dba56faf0ba8b4a0615bfbfb31418
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-05 01:53:20 +0200

    Job Listing and display job dynamically from mock json data

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import NavBar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
 </script>
 <template>
   <NavBar />
   <Hero title="Find Your Dream Job" />
+  <HomeCards />
+  <JobListings />
 </template>
---
+++ vue-project/src/components/JobListings.vue
@@ -0,0 +1,20 @@
+<script setup>
+import jobData from "@/jobs.json";
+import { ref } from "vue";
+import JobListing from "./JobListing.vue";
+
+const jobs = ref(jobData.jobs);
+console.log(jobs.value);
+</script>
+<template>
+  <section class="bg-green-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+      </div>
+    </div>
+  </section>
+</template>
