ffa76636c971fcd217d8e4b49667c2617bd78044
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-05 00:20:41 +0200

    Add static template of Nav and hero section

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,3 +1,8 @@
+<script setup>
+import NavBar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+</script>
 <template>
-  <h1 class="text-2xl">Jobs</h1>
+  <NavBar />
+  <Hero />
 </template>
