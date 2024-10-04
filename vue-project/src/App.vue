8dd67ad24237e11cd85278084897e0ff5c38d6cc
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-04 20:12:55 +0200

    composition api

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,21 +1,31 @@
-<script>
-export default {
-  data() {
-    return {
-      name: "Ashiqur Russel",
-      profession: "Full Stack Developer",
-      company: "bidi Bilding Digital GmbH",
-      status: "inactive",
-    };
-  },
+<script setup>
+import { ref } from "vue";
+
+const name = ref("Ashiqur Russel");
+const status = ref("active");
+const tasks = ref(["Task One", "Task Two", "Task three"]);
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
 };
 </script>
 
 <template>
   <h1>Name: {{ name }}</h1>
-  <p>Company: {{ company }}</p>
-  <p>Role: {{ profession }}</p>
   <p v-if="status === 'active'">Status: Employee is Active.</p>
   <p v-else-if="status == 'pending'">Status: Employee is Pending.</p>
-  <p v-else="status === 'inactive'">Status: Employee is Inactive.</p>
+  <p v-else>Status: Employee is Inactive.</p>
+
+  <h3>Tasks:</h3>
+  <ul v-for="task in tasks" :key="task">
+    <li>{{ task }}</li>
+  </ul>
+
+  <button @click="toggleStatus">Change Status</button>
 </template>
---
+++ vue-project/src/App2.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Ashiqur Russel",
+      profession: "Full Stack Developer",
+      company: "bidi Bilding Digital GmbH",
+      status: "inactive",
+      tasks: ["Task One", "Task Two", "Task three"],
+      link: "https://www.google.com",
+    };
+  },
+
+  methods: {
+    toggleStatus() {
+      if (this.status === "active") {
+        this.status = "pending";
+      } else if (this.status === "pending") {
+        this.status = "inactive";
+      } else {
+        this.status = "active";
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Name: {{ name }}</h1>
+  <p>Company: {{ company }}</p>
+  <p>Role: {{ profession }}</p>
+  <p v-if="status === 'active'">Status: Employee is Active.</p>
+  <p v-else-if="status == 'pending'">Status: Employee is Pending.</p>
+  <p v-else>Status: Employee is Inactive.</p>
+  <a :href="link">{{ link }}</a>
+
+  <h3>Tasks:</h3>
+  <ul v-for="task in tasks" :key="task">
+    <li>{{ task }}</li>
+  </ul>
+
+  <button @click="toggleStatus">Change Status</button>
+</template>
