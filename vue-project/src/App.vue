f2f619eadd7d99a9e35a6fa1596b3f0ed2d3f63c
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-04 18:22:57 +0200

    Conditiona directive v-if

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,3 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Ashiqur Russel",
+      profession: "Full Stack Developer",
+      company: "bidi Bilding Digital GmbH",
+      status: "inactive",
+    };
+  },
+};
+</script>
+
 <template>
-  <h1>Hello Vue Jobs</h1>
+  <h1>Name: {{ name }}</h1>
+  <p>Company: {{ company }}</p>
+  <p>Role: {{ profession }}</p>
+  <p v-if="status === 'active'">Status: Employee is Active.</p>
+  <p v-else-if="status == 'pending'">Status: Employee is Pending.</p>
+  <p v-else="status === 'inactive'">Status: Employee is Inactive.</p>
 </template>
