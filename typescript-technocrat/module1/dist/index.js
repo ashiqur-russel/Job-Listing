7f138768aed1b1fd08902c091edb96d84b690e4f
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-07 01:26:36 +0200

    type check , interface create, object user create

--- typescript-technocrat/module1/dist/index.js
+++ typescript-technocrat/module1/dist/index.js
@@ -1,3 +1,15 @@
 "use strict";
 let course = "hello typescript";
-console.log(course);
+const user1 = {
+    firstName: "Ash",
+    lastName: "Russ",
+    age: 100,
+};
+const user2 = {
+    firstName: "John",
+    lastName: "Sina",
+    age: 99,
+    isEmployed: true,
+    managerName: "Big Shaw",
+    company: "bidi Bildung",
+};
--- typescript-technocrat/module1/src/index.ts
+++ typescript-technocrat/module1/src/index.ts
@@ -1,2 +1,25 @@
 let course: string = "hello typescript";
-console.log(course);
+
+interface IUser {
+  readonly managerName?: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  isEmployed?: boolean;
+  company?: "bidi Bildung";
+}
+
+const user1: IUser = {
+  firstName: "Ash",
+  lastName: "Russ",
+  age: 100,
+};
+
+const user2: IUser = {
+  firstName: "John",
+  lastName: "Sina",
+  age: 99,
+  isEmployed: true,
+  managerName: "Big Shaw",
+  company: "bidi Bildung",
+};
