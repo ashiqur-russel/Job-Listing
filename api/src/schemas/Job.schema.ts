8e30836b7aa990d3247d020d5afc4f1054eb90d4
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-07 20:02:06 +0200

    Create Job schema

--- api/src/schemas/Job.schema.ts
+++ api/src/schemas/Job.schema.ts
@@ -1,9 +1,34 @@
-import { Prop, Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type JobDocument = Job & Document;
+
+export type Company = {
+  name: string;
+  description: string;
+  contactEmail: string;
+  contactPhone: string;
+};
 
 @Schema()
 export class Job {
-  
-    @Prop({ required: true})
-    type:
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  type: string;
+
+  @Prop({ required: true })
+  location: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  salary: string;
+
+  @Prop({ type: Object, required: true })
+  company: Company;
 }
 
+export const JobSchema = SchemaFactory.createForClass(Job);
