93429531dac3188c4c213df3cc171f36821038b8
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-08 01:45:13 +0200

    Fix bug

--- vue-project/src/views/Job.vue
+++ vue-project/src/views/Job.vue
@@ -104,7 +104,7 @@ onMounted(async () => {
           <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-xl font-bold mb-6">Manage Job</h3>
             <RouterLink
-              :to="`/jobs/edit/${state.job.id}`"
+              :to="`/jobs/edit/${state.job._id}`"
               class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >Edit Job</RouterLink
             >
