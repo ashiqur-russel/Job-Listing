b1b778d6833ae327b81134c756640bae6c3d12f8
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-08 03:30:42 +0200

    remove express adapter

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,18 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import { Handler } from 'aws-lambda';
-import * as express from 'express';
 import serverless from 'serverless-http';
 import { AppModule } from './app.module';
 
-const expressApp = express();
-
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
