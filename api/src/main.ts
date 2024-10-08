2a75429b687906b50a5694a689bcb46ba6791ffd
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-08 03:33:18 +0200

    remove unesessary files

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,24 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { Handler } from 'aws-lambda';
-import serverless from 'serverless-http';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.useGlobalPipes(new ValidationPipe());
-
-  const port = process.env.PORT || 5000;
-
-  if (process.env.NODE_ENV !== 'production') {
-    await app.listen(port);
-    console.log(`Application is running on: http://localhost:${port}`);
-  } else {
-    await app.init();
-  }
+  await app.listen(5000);
 }
-
 bootstrap();
-
-export const handler: Handler = serverless(expressApp);
