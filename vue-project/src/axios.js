a5e5a44fccf9534882b7416286d305a043ad03b8
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-10 01:01:57 +0200

    Production API url comment out

--- vue-project/src/axios.js
+++ vue-project/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //baseURL: import.meta.env.VITE_API_URL,
-  baseURL: import.meta.env.VITE_API_URL_DEV,
+  baseURL: import.meta.env.VITE_API_URL,
+  //baseURL: import.meta.env.VITE_API_URL_DEV,
 });
 
 export default instance;
