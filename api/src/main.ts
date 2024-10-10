f53939cf9d914233eb439e69828f0669e90536d4
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-11 00:53:09 +0200

    check cours issue

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,31 +8,28 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = [
-    process.env.FRONTEND_URL,
-    process.env.FRONTEND_URL_PRODUCTION,
-  ].filter(Boolean);
+  const allowedOrigins = [process.env.FRONTEND_URL_PRODUCTION].filter(Boolean);
+
+  app.enableCors({
+    origin: ['https://find-job-eight.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
 
   app.use((req, res, next) => {
-    res.header(
-      'Access-Control-Allow-Origin',
-      'https://find-job-eight.vercel.app',
-    );
+    res.header('Access-Control-Allow-Origin', allowedOrigins);
     res.header(
       'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     );
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     );
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
     next();
-  });
-
-  app.enableCors({
-    origin: allowedOrigins,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
   });
 
   // global validation
