e1c7c07a9de5bbce50ddb4d50ea18292cab75633
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-11 01:13:17 +0200

    .

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,37 +8,20 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = [process.env.FRONTEND_URL_PRODUCTION].filter(Boolean);
+  const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    process.env.FRONTEND_URL_PRODUCTION,
+  ].filter(Boolean);
 
   app.enableCors({
-    origin: ['http://find-job-eight.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTION',
     credentials: true,
-  });
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', allowedOrigins);
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    );
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    );
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(200);
-    }
-    next();
+    allowedHeaders: 'Content-Type, Authorization',
   });
 
   // global validation
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
--- vue-project/src/axios.js
+++ vue-project/src/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   //baseURL: import.meta.env.VITE_API_URL_DEV,
+  withCredentials: true,
 });
 
 export default instance;
