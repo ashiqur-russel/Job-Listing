58143dffea2fab71485ed0d0cff63a17808d0868
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-11 14:50:41 +0200

    check cors issue

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,13 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cors from 'cors';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{{ cors: true }});
-  
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(cors());
+
   // global validation
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 5000);
