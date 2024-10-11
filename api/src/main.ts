b48d0164eb6f3a2d6b5449bd0ce7a50de8174f19
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-11 13:44:16 +0200

    .

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,20 +4,32 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cors from 'cors';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const corsOptions = {
-    origin: '*',
-  } as CorsOptions;
+  const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    process.env.FRONTEND_URL_PRODUCTION,
+  ].filter(Boolean);
 
-  app.enableCors(corsOptions);
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+      allowedHeaders: 'Content-Type, Authorization',
+    }),
+  );
 
-  // global validation
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT || 5000);
