90bd81c95d6e60e3b5eb463427336bf7f2db909c
Ashiqur <ashiqur.rahman@bidi.one>
2024-10-11 14:54:01 +0200

    .

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,12 +2,27 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cors from 'cors';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
+
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://find-job-eight.vercel.app',
+    '*',
+  ];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   app.use(cors());
 
   // global validation
